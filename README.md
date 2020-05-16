625ecff3b540226bdf1ad6c42f96c03c89413589
Jil Sahm <j.sahm@gmx.de>
2020-05-16 08:10:52 +0200

    init: Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Prequesitions
+
+* activate the tampermonker addon for Firefox
+* connect your twitch account with your riot account
+
+## Usage
+
+Just open a stream with drops enabled. Now wait and maybe you have some luck. It took me three evenings after i got mine.
---
+++ src/grinder.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Twitch Clicker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Jil Sahm
+// @match        www.twitch.tv/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(check, 3000);
+})();
+
+function check() {
+    'use strict';
+    var button = document.querySelector("button.tw-button");
+    if (button) {
+        console.log("Will click button");
+        button.click();
+    } else {
+        console.log("Button not available")
+    }
+}
